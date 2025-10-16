dc61f09d2fe5e0b8d37f324fd896ae4861947eae
Mohith K.C <mohith@sharaaninfo.com>
2025-10-16 13:06:41 +0530

    local ip configured

--- constant.js
+++ constant.js
@@ -1,3 +1,3 @@
-//export const BASE_URL='http://192.168.10.65:9000'
+export const BASE_URL='http://192.168.10.65:9000'
 
-export const BASE_URL='https://mis-backend-ruddy.vercel.app'
+//export const BASE_URL='https://mis-backend-ruddy.vercel.app'
